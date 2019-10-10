cc24b38edfe38cc8378cbe28242a7ddbc5cf0cbe
Diego Castillo <raigiku@hotmail.com>
2019-10-09 19:10:38 -0500

    Se agrego la lectura del archivo input

---
+++ 3D_Packing/Assets/Scripts/Components/IdComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Packing_3D.Components
+{
+    public class IdComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private List<TMP_Text> idTexts = new List<TMP_Text>();
+
+        public void ChangeIdText(string id)
+        {
+            idTexts.ForEach(idText => idText.text = id);
+        }
+    }
+}
---
+++ 3D_Packing/Assets/Scripts/Components/ReaderComponent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Packing_3D.Models;
+
+namespace Packing_3D.Components
+{
+    public class ReaderComponent : MonoBehaviour
+    {
+        private Dictionary<string, Format> formats;
+        private Vector3 containerSize;
+
+        public void ClearEntities()
+        {
+            formats = new Dictionary<string, Format>();
+        }
+
+        public void ReadInputFile()
+        {
+            var input_lines = File.ReadAllLines(@"Assets\input.txt");
+            ParseInputContainer(input_lines[0].Split(' '));
+            ParseInputFormats(input_lines);
+        }
+
+        private void ParseInputContainer(string[] dimensions)
+        {
+            var height = float.Parse(dimensions[0]);
+            var width = float.Parse(dimensions[1]);
+            var length = float.Parse(dimensions[2]);
+            containerSize = new Vector3(width, height, length);
+        }
+
+        private void ParseInputFormats(string[] input_lines)
+        {
+            var total_formats = int.Parse(input_lines[1]);
+            for (var i = 2; i < total_formats + 2; ++i)
+            {
+                var properties = input_lines[i].Split(' ');
+                var totalBlocks = int.Parse(properties[0]);
+                var id = properties[1];
+                var height = float.Parse(properties[2]);
+                var width = float.Parse(properties[3]);
+                var length = float.Parse(properties[4]);
+
+                formats[id] = new Format
+                {
+                    Id = id,
+                    Size = new Vector3(width, height, length),
+                    Blocks = new List<Block>()
+                };
+
+                for (var j = 0; j < totalBlocks; ++j)
+                {
+                    formats[id].Blocks.Add(new Block
+                    {
+                        Position = Vector3.zero,
+                        Container = null,
+                        Format = formats[id]
+                    });
+                }
+            }
+        }
+    }
+}
